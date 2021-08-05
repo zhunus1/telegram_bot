06c233b978a09694b4c9310e7096426a2e0d2395
Zhunus <170107102@stu.sdu.edu.kz>
2021-08-05 15:57:52 +0600

    'Added_pagination'

--- bot.py
+++ bot.py
@@ -25,74 +25,78 @@ def welcome(message):
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower():
-        results = search_drug(message.text.lower())
-        if len(results) == 0:
+        drug_name = message.text.lower()
+        results = search_drug(drug_name)
+        if len(results) == 0: #Если нет лекарства
             bot.send_message(message.from_user.id, 'К сожалению в базе отсутствуют данные касательно данного препарата.')
             bot.send_message(message.from_user.id, 'Попробуйте повторить поиск по другому названию')
         else:
-            pagination = DrugPaginationModel(results, 4)
-            reply_markup = create_menu(pagination.page_items, 'name', 'id')
-
+            reply_markup = create_menu(results, 'name', 'id')
             image_url = get_image(results[0]['imageName'])
+
             bot.send_photo(message.from_user.id, image_url)
-            bot.send_message(message.from_user.id, '*Название: %s*' % results[0]['name'], parse_mode="Markdown")
-            bot.send_message(message.from_user.id, '*Цена: %s*' % results[0]['price'], parse_mode="Markdown")
-            load_more = types.InlineKeyboardButton("Загрузи больше", callback_data = "%s,%s,%s" % (message.text.lower(), "load_more", str(pagination.current_page_number)))
-            reply_markup.add(load_more)
-            bot.send_message(message.from_user.id, 'Результаты поиска: %s' % message.text, reply_markup=reply_markup)
+            bot.send_message(message.from_user.id, '*Название: %s*' % results[0]['name'], parse_mode = "Markdown")
+            bot.send_message(message.from_user.id, '*Цена: %s*' % results[0]['price'], parse_mode = "Markdown")
+            bot.send_message(message.from_user.id, 'Результаты поиска: %s' % message.text, reply_markup = reply_markup)
 
-
+#Обрабатываем нажатия на кнопки
+pagination = None
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
+    global pagination
     try:
-        if call.data.isdigit(): #Проверка если это кнопка лекарства
-            result = drug_detailed(call.data)
+        #Проверка если это кнопка препарата в меню
+        if call.data.isdigit():
+            drug_id = call.data
+            result = drug_detailed(drug_id)
             image_url = get_image(result['imageName'])
             bot.send_photo(call.message.chat.id, image_url)
 
             button_list = []
             markup = types.InlineKeyboardMarkup(row_width=2)
             clean_data = drug_detailed_data_clean(result)
-            for key in clean_data:
+
+            for parameter in clean_data:
                 button_list.append(types.InlineKeyboardButton(
-                    key,
-                    callback_data = key + ',' + call.data,
+                    parameter,
+                    callback_data = parameter + ',' + drug_id,
                 ))
             markup.add(*button_list)
 
-            bot.send_message(call.message.chat.id, '*Название: %s*' % result['name'], parse_mode="Markdown")
-            bot.send_message(call.message.chat.id, '*Цена: %s*' % result['price'], parse_mode="Markdown")
-            bot.send_message(call.message.chat.id, '*Отпуск: %s*' % result['dispensedByPrescription'], parse_mode="Markdown")
-            bot.send_message(call.message.chat.id, 'Подробная информация', reply_markup=markup)
-        elif 'load_more' in call.data.split(','):
-            data = call.data.split(',')
-            results = search_drug(data[0])
-            pagination = DrugPaginationModel(results, 4)
-            pagination._current_page = int(data[2])
+            bot.send_message(call.message.chat.id, '*Название: %s*' % result['name'], parse_mode = "Markdown")
+            bot.send_message(call.message.chat.id, '*Цена: %s*' % result['price'], parse_mode = "Markdown")
+            bot.send_message(call.message.chat.id, '*Отпуск: %s*' % result['dispensedByPrescription'], parse_mode = "Markdown")
+            bot.send_message(call.message.chat.id, 'Подробная информация', reply_markup = markup)
+
+        #Проверка если это кнопка Загрузить больше лекарств
+        elif 'load_more' in call.data:
+
             if pagination.has_next_page:
                 pagination.flip_next_page()
 
                 reply_markup = create_menu(pagination.page_items, 'name', 'id')
-                load_more = types.InlineKeyboardButton("Загрузи больше", callback_data = "%s,%s" % (data[0], "load_more"))
+                load_more = types.InlineKeyboardButton("Загрузи больше", callback_data = "load_more")
                 reply_markup.add(load_more)
-                bot.send_message(call.message.chat.id, 'Результаты поиска: %s' % data[0], reply_markup=reply_markup)
+                bot.send_message(call.message.chat.id, 'Страница №%s' % str(pagination.current_page_number), reply_markup=reply_markup)
             else:
                 bot.send_message(call.message.chat.id, 'Больше нет данных')
-        else:#Проверка если это кнопка параметра
+
+        #Проверка если это кнопка параметра препарата
+        else:
             results, drug_id, drug_name = get_param(call.data)
             button_list = []
             markup = types.InlineKeyboardMarkup(row_width=2)
-            if isinstance(results, list):#проверка если кликнули аналоги
+            #Проверка если кликнули на кнопку Аналоги
+            if isinstance(results, list):
                 pagination = DrugPaginationModel(results, 4)
                 reply_markup = create_menu(pagination.page_items, 'name', 'id')
 
-                load_more = types.InlineKeyboardButton("Загрузи больше", callback_data = "%s,%s,%s" % (drug_name, "load_more", str(pagination.current_page_number)))
+                load_more = types.InlineKeyboardButton("Загрузи больше", callback_data = "load_more")
                 reply_markup.add(load_more)
 
-                bot.send_message(call.message.chat.id, 'Список аналогов', reply_markup=reply_markup)
+                bot.send_message(call.message.chat.id, 'Страница №%s' % str(pagination.current_page_number), reply_markup=reply_markup)
             else:
                 bot.send_message(call.message.chat.id, results)
-            #Также нужно уловить случай с аналогом
 
 
     except Exception as e:
